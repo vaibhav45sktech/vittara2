39e10498c00f8534f138a850b8b9db73cc982288
Affan Raza <affanraza8081@gmail.com>
2025-12-24 01:15:09 +0530

    fix: resolve final lint errors in db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -19,11 +19,10 @@ interface MongooseCache {
 }
 
 declare global {
-  // eslint-disable-next-line no-var
   var mongoose: MongooseCache | undefined;
 }
 
-let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+const cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
 if (!global.mongoose) {
   global.mongoose = cached;
